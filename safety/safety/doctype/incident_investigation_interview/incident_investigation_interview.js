d6af3faa43534c7cacb8b2af975c740c6b5aa32b
buff0k <buff0k@gmail.com>
2026-07-17 15:53:21 +0200

    modified:   safety/__init__.py 	modified:   safety/safety/doctype/incident_investigation_interview/incident_investigation_interview.js 	modified:   safety/safety/doctype/incident_investigation_interview/incident_investigation_interview.json 	modified:   safety/safety/doctype/incident_investigation_interview/incident_investigation_interview.py 	modified:   safety/safety/doctype/incident_report/incident_report.json Workflow tweaks to new Investigation Process

--- safety/safety/doctype/incident_investigation_interview/incident_investigation_interview.js
+++ safety/safety/doctype/incident_investigation_interview/incident_investigation_interview.js
@@ -3,33 +3,11 @@
 
 frappe.ui.form.on("Incident Investigation Interview", {
     refresh(frm) {
-        toggle_interviewee_fields(frm);
-    },
-
-    external_interviewee(frm) {
-        toggle_interviewee_fields(frm);
-    },
-
-    interviewee_employee(frm) {
-        if (!frm.doc.interviewee_employee) {
-            return;
+        if (frm.doc.docstatus === 0 && !frm.doc.statement_file) {
+            frm.dashboard.set_headline_alert(
+                __("Attach the signed interview statement before submitting."),
+                "orange"
+            );
         }
-
-        frappe.db.get_value(
-            "Employee",
-            frm.doc.interviewee_employee,
-            ["employee_name", "designation"]
-        ).then((r) => {
-            const employee = r.message || {};
-            frm.set_value("interviewee_designation", employee.designation);
-        });
-    },
+    }
 });
-
-function toggle_interviewee_fields(frm) {
-    const external = Boolean(frm.doc.external_interviewee);
-    frm.toggle_display("interviewee_employee", !external);
-    frm.toggle_reqd("interviewee_employee", !external);
-    frm.toggle_display("external_interviewee_name", external);
-    frm.toggle_reqd("external_interviewee_name", external);
-}
